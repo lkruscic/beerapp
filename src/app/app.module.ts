bd226e40275a16bbaf08c3731621eaf9f5d4d59f
Luka Kruscic <>
2017-08-22 20:07:13 +0200

    Added beer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BeerComponent } from './beer/beer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BeerComponent
   ],
   imports: [
     BrowserModule,
